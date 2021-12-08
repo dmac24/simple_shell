15bd1ab729594dae326a3f7c1915e7985fbd7e57
maycolroa <lehallo@hotmail.com>
2021-12-08 02:43:49 -0500

    0x16. C - Simple Shell  0.08 08/12/21

---
+++ tokenizer.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * tokenizer - Analyze arguments in tokens.
+ *
+ * @BUFF: bring arguments.
+ *
+ * Return: Arg_str.
+ */
+
+char **tokenizer(char *BUFF)
+{
+	char *token = NULL;
+	char **Arg_str = NULL;
+	int i = 0, size = 0;
+
+	while (BUFF[size] != '\0')
+		size++;
+
+	Arg_str = malloc(sizeof(char *) * size);
+
+	i = 0;
+
+	token = strtok(BUFF, DELIM);
+	Arg_str[i] = token;
+
+	for (i = 1; token != NULL; i++)
+	{
+		token = strtok(NULL, DELIM);
+		Arg_str[i] = token;
+	}
+
+	return (Arg_str);
+}
