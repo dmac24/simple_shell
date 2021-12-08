ebd11326ddd30d53ae4c92d050ac50c3ebf08c7e
dmac24 <dinitayala@gmail.com>
2021-12-08 07:42:45 +0000

    shell 0x08

---
+++ shell.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - start shell.
+ *
+ * Return: (0).
+ */
+
+int main(void)
+{
+	char *row = NULL;
+	char **tokens = NULL;
+	int ct_output = 0;
+
+	while (1)
+	{
+		write(STDOUT, "#holbie$ ", 9);
+
+		row = rd_row();
+		tokens = tokenizer(row);
+		ct_output = Run(tokens, ct_output, row);
+
+		free(row);
+		free(tokens);
+	}
+
+	return (0);
+}
